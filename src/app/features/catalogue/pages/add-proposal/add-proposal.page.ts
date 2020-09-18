ee519fdc3b43696186c4effcb9c267b3d46ae5e8
Franck106 <francois612-resa@yahoo.fr>
2020-09-18 19:40:07 +0200

    FRONT-3 add Proposal page

--- src/app/features/catalogue/pages/add-proposal/add-proposal.page.ts
+++ src/app/features/catalogue/pages/add-proposal/add-proposal.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SimpleAuthenticationService } from 'src/app/features/authentication/services/simple-authentication.service';
 import { CatalogueService } from '../../services/catalogue.service';
 import { Category } from '../../services/models/category';
+import { Proposal } from '../../services/models/proposal';
 
 @Component({
   templateUrl: './add-proposal.page.html',
@@ -10,25 +13,55 @@ import { Category } from '../../services/models/category';
 })
 export class AddProposalPage implements OnInit {
 
-  form = new FormControl(
-    {
-      description: '',
-      prix: '',
-      dictance: ''
-    }
-  );
+  form = this.formBuilder.group({
+    category:['', Validators.required],
+    name:['', Validators.required],
+    description: ['', Validators.required],
+    price: ['', Validators.required],
+    maxDistance: ['', Validators.required],
+  });
+
 
   categories$: Observable<Category[]>;
 
-  constructor(private catalogue: CatalogueService) { }
+  constructor(private catalogue: CatalogueService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private auth: SimpleAuthenticationService) { }
 
   ngOnInit(): void {
     this.categories$ = this.catalogue.getCategories();
   }
 
-  addProposal(): void {
-    console.log('addProposal'+ this.form.value);
-    this.catalogue.addProposal(this.form.value);
+  onSubmit(): void {
+    console.warn(this.form.value);
+    const proposal: Proposal = {
+      name: this.form.value.name,
+      description: this.form.value.description,
+      price: this.form.value.price,
+      category: this.form.value.category,
+      provider: this.auth.getConnectedUser(),
+      maxDistance: this.form.value.maxDistance,
+      date: new Date()
+    };
+    console.log(proposal);
+    this.catalogue.addProposal(proposal).subscribe(
+      (response) => {
+        if(response == null) {
+          console.error('Server error');
+        } else {
+          console.log(response);
+          this.router.navigateByUrl('/');
+        }
+      }
+    );
+  }
+
+  demo() {
+    this.form.value.description = 'bla';
+    this.form.value.name = 'blo';
+    this.form.value.price = 4;
+    this.form.value.maxDistance=3;
   }
 
 }
