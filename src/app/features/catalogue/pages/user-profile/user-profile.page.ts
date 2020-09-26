275519a407eaa436dc50c1fede6ac75466fa396d
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-09-26 10:06:49 +0200

    Add chatbox (local conf)

--- src/app/features/catalogue/pages/user-profile/user-profile.page.ts
+++ src/app/features/catalogue/pages/user-profile/user-profile.page.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { User } from '../../services/models/user';
-import { CatalogueService } from '../../services/catalogue.service';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Input } from '@angular/core';
-import { switchMap } from 'rxjs/operators';
+import {ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {User} from '../../services/models/user';
+import {CatalogueService} from '../../services/catalogue.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {SimpleAuthenticationService} from '../../../authentication/services/simple-authentication.service';
 
+declare var createChatBox: (rootElement: HTMLElement | null, sourceId: string, targetId: string) => void;
 
 @Component({
   templateUrl: './user-profile.page.html',
@@ -14,20 +14,30 @@ import { switchMap } from 'rxjs/operators';
 })
 export class UserProfilePage implements OnInit {
 
-  provider : Observable<User | undefined>;
+  userId: number;
+  myUserId: number;
 
-  constructor(private catalogue: CatalogueService, private route: ActivatedRoute) { }
+  shouldShowChatBox: boolean;
 
-  ngOnInit(): void {
-  this.provider = this.catalogue.getUserById(parseInt(this.route.snapshot.paramMap.get('id') || '1'));
- 
+  @ViewChild('chatBox')
+  set chatBox(elem: ElementRef) {
+    if (!! elem) {
+      this.myUserId = this.auth.getConnectedUser().id || -1;
+      createChatBox(elem.nativeElement, this.myUserId.toString(), this.userId.toString());
+    }
   }
 
- 
-  
-  
+  provider: Observable<User | undefined>;
 
-  
+  constructor(private catalogue: CatalogueService, private route: ActivatedRoute,
+              private auth: SimpleAuthenticationService) {
+  }
 
+  ngOnInit(): void {
+    this.userId = parseInt(this.route.snapshot.paramMap.get('id') || '1', 10);
+    this.provider = this.catalogue.getUserById(this.userId);
 
+    this.shouldShowChatBox = this.auth.getConnectedUser() != null &&
+      this.auth.getConnectedUser().id !== this.userId;
+  }
 }
