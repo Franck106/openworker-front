2070ca1a749513510efae2dd352ae4d8d43d623d
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-09-27 15:21:22 +0200

    Maps: add focus to card when a marker is clicked

--- src/app/features/catalogue/components/proposal-item/proposal-item.component.ts
+++ src/app/features/catalogue/components/proposal-item/proposal-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ChangeDetectionStrategy, Input} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, Input, ChangeDetectorRef, ElementRef} from '@angular/core';
 import {Proposal} from '../../services/models/proposal';
 import {Router} from '@angular/router';
 
@@ -14,7 +14,12 @@ export class ProposalItemComponent implements OnInit {
 
   @Input() showContactButton: boolean;
 
-  constructor(private router: Router) {
+  focused: boolean;
+  onTop: boolean;
+
+  constructor(private router: Router,
+              private cdr: ChangeDetectorRef,
+              private element: ElementRef) {
   }
 
   ngOnInit(): void {
@@ -22,5 +27,19 @@ export class ProposalItemComponent implements OnInit {
 
   contactClicked(proposal: Proposal): void {
     this.router.navigateByUrl(`/catalogue/user/${proposal.provider.id}?proposal=${proposal.id}`);
+  }
+
+  focus(): void {
+    this.focused = true;
+    this.onTop = true;
+    this.element.nativeElement.scrollIntoView({behavior: 'smooth'});
+    this.cdr.detectChanges();
+
+    setTimeout(() => {
+      this.focused = false;
+      this.cdr.detectChanges();
+
+      setTimeout(() => this.onTop = false, 1000);
+    }, 1000);
   }
 }
