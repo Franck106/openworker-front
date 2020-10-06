1ab83c0e949fd21d43ade42460196df4959bfd7a
Franck106 <francois612-resa@yahoo.fr>
2020-10-06 20:01:14 +0200

    scrapper improve

--- src/app/features/scrapper/scrapper.component.ts
+++ src/app/features/scrapper/scrapper.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ScrapperUser } from 'src/app/core/elastic-search/scrapper-user';
+import { compose, map, orderBy, reduce, slice, uniq } from 'lodash/fp'; 
 
 @Component({
   selector: 'app-scrapper',
@@ -8,7 +9,7 @@ import { ScrapperUser } from 'src/app/core/elastic-search/scrapper-user';
 })
 export class ScrapperComponent implements OnInit {
 
-  @Input() users: ScrapperUser[];
+  @Input() users: ScrapperUser[] = [];
   skills: string[];
   averagePrice: number;
 
@@ -16,6 +17,8 @@ export class ScrapperComponent implements OnInit {
 
   ngOnInit(): void {
     this.averagePrice = this.getAveragePrice(this.users);
+    this.skills = this.getTopSkills(this.users);
+    console.log(this.skills);
   }
   
   getAveragePrice(users: ScrapperUser[]): number {
@@ -24,6 +27,31 @@ export class ScrapperComponent implements OnInit {
     }, 0);
     return total / users.length;
   }
+
+  //List of all the skills in the user list
+  allSkills = this.users.map((user) => user._source.skills.map((skill) => skill));
+
+
+
+  getTopSkills(users: ScrapperUser[]): [string, string, string] {
+    return compose(
+      slice(0, 3),
+      uniq,
+      map<{ skill: string; count: number }, string>(({ skill }) => skill),
+      orderBy<{ skill: string; count: number }>(["count"], ["desc"]),
+      (arr: string[]) => reduce<string, { skill: string; count: number }[]>((acc, item) => {
+        return [
+          ...acc,
+          {
+            skill: item,
+            count: arr.filter((_) => _ === item).length,
+          },
+        ];
+      }, [])(arr),
+      reduce<ScrapperUser, string[]>((acc,item) => [...acc, ...item._source.skills], [])
+    )(users);
+  }
+    
   
   
 
